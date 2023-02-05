ad5b75178ec7fb7f5d660fd99f16050e71046665
ibu0125 <ibu01257248@gmail.com>
2023-02-05 19:08:39 +0900

    フィボナッチ数列

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hellaaaaaao,a waorld!");
+    let mut n1 = 1;
+    let mut n2: i32 =1;
+    let mut n3: i32;
+    println!("{}",n1);
+    println!("{}",n2);
+    for _i in 1..=30 {
+        n3=n1+n2;
+        println!("{}",n3);
+        n1=n2;
+        n2=n3;
+    }
 }
